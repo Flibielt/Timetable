a9fca1c85a86fdb809ee00f2c7733bee84a48816
flibielt <hcsabi98@gmail.com>
2019-12-07 22:21:10 +0100

    LessonFragment onCreateView function created

--- app/src/main/java/com/example/timetable/lesson/LessonFragment.kt
+++ app/src/main/java/com/example/timetable/lesson/LessonFragment.kt
@@ -4,7 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.timetable.R
+import com.example.timetable.database.TimetableDatabase
+import com.example.timetable.databinding.LessonFragmentBinding
 
 class LessonFragment : Fragment() {
 
@@ -13,7 +18,19 @@ class LessonFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //todo: Override onCreateView
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val binding: LessonFragmentBinding = DataBindingUtil.inflate(inflater, R.layout.lesson_fragment, container, false)
+        val application = requireNotNull(this.activity).application
+
+        val lessonDataSource = TimetableDatabase.getInstance(application).lessonDao
+        val timetableDataSource = TimetableDatabase.getInstance(application).timetableDao
+        val viewModelFactory = LessonViewModelFactory(lessonDataSource, timetableDataSource, application)
+
+        val lessonViewModel = ViewModelProviders.of(this, viewModelFactory).get(LessonViewModel::class.java)
+        binding.lessonViewModel = lessonViewModel
+
+        //todo: Show snackbar (optional)
+        //todo: navigation
+
+        return binding.root
     }
 }
