35fa70d6a7fea2fc3b46b816c621fed845f58d2a
flibielt <hcsabi98@gmail.com>
2019-12-07 21:41:11 +0100

    Timetable fragment improvements

--- app/src/main/java/com/example/timetable/timetable/TimetableFragment.kt
+++ app/src/main/java/com/example/timetable/timetable/TimetableFragment.kt
@@ -1,2 +1,40 @@
 package com.example.timetable.timetable
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.example.timetable.R
+import com.example.timetable.database.TimetableDatabase
+import com.example.timetable.databinding.TimetableFragmentBinding
+import com.google.android.material.snackbar.Snackbar
+
+class TimetableFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) : View? {
+        val binding: TimetableFragmentBinding = DataBindingUtil.inflate(
+            inflater, R.layout.timetable_fragment, container,false)
+
+        val application = requireNotNull(this.activity).application
+
+        val timetableDatasource = TimetableDatabase.getInstance(application).timetableDao
+        val lessonDatasource = TimetableDatabase.getInstance(application).lessonDao
+
+        val viewModelFactory = TimetableViewModelFactory(timetableDatasource, lessonDatasource, application)
+        val timetableViewModel = ViewModelProviders.of(this, viewModelFactory).get(TimetableViewModel::class.java)
+
+        binding.timetableViewModel = timetableViewModel
+        binding.setLifecycleOwner(this)
+
+        //todo: navigation
+
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/timetable/timetable/TimetableViewModelFactory.kt
+++ app/src/main/java/com/example/timetable/timetable/TimetableViewModelFactory.kt
@@ -1,2 +1,21 @@
 package com.example.timetable.timetable
 
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.timetable.database.LessonDao
+import com.example.timetable.database.TimetableDao
+import java.lang.IllegalArgumentException
+
+class TimetableViewModelFactory (
+    private val timetableDatasource: TimetableDao,
+    private val lessonDatasource: LessonDao,
+    private val application: Application) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(TimetableViewModel::class.java)) {
+            return TimetableViewModel(timetableDatasource, lessonDatasource, application) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
